63842500604b37ea810b655b29693a6cffd4bfb3
Prem <insomn.studio@gmail.com>
2026-09-12 17:08:59 +0530

    simple grep app with rust

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use minigrep::{search, search_case_insensitive};
+use std::{env, error::Error, fs, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Failed to parse arguments: {err}");
+        process::exit(1);
+    });
+    if let Err(err) = run(config) {
+        eprintln!("Application error: {err}");
+        process::exit(1);
+    };
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path).unwrap_or_else(|err| {
+        eprintln!("An error occured while reading the file:\nError: {}", err);
+        process::exit(1);
+    });
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in results {
+        println!("-> {line}");
+    }
+    return Ok(());
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        return Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        });
+    }
+}
